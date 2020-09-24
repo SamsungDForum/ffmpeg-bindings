13389a4a5dbe4614a31e9a559bfd503b5bac9dcb
Pawel Flis <p.flis@samsung.com>
2020-09-24 16:24:13 +0200

    initial commit

---
+++ FFmpegBindings/Interop/FFmpeg.structs.incomplete.cs
@@ -0,0 +1,119 @@
+/*!
+ * https://github.com/SamsungDForum/JuvoPlayer
+ * Copyright 2018, Samsung Electronics Co., Ltd
+ * Licensed under the MIT license
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+namespace FFmpegBindings.Interop
+{
+    public unsafe struct AVAudioFifo
+    {
+    }
+
+    public unsafe struct AVBPrint
+    {
+    }
+
+    public unsafe struct AVBuffer
+    {
+    }
+
+    public unsafe struct AVBufferPool
+    {
+    }
+
+    public unsafe struct AVDictionary
+    {
+    }
+
+    public unsafe struct SwrContext
+    {
+    }
+
+    public unsafe struct SwsContext
+    {
+    }
+
+    public unsafe struct AVCodecDefault
+    {
+    }
+
+    public unsafe struct AVCodecInternal
+    {
+    }
+
+    public unsafe struct MpegEncContext
+    {
+    }
+
+    public unsafe struct ReSampleContext
+    {
+    }
+
+    public unsafe struct AVBSFInternal
+    {
+    }
+
+    public unsafe struct AVBSFList
+    {
+    }
+
+    public unsafe struct AVResampleContext
+    {
+    }
+
+    public unsafe struct AVStreamInternal
+    {
+    }
+
+    public unsafe struct FFFrac
+    {
+    }
+
+    public unsafe struct AVFormatInternal
+    {
+    }
+
+    public unsafe struct AVCodecTag
+    {
+    }
+
+    public unsafe struct URLContext
+    {
+    }
+
+    public unsafe struct AVFilterPad
+    {
+    }
+
+    public unsafe struct AVFilterFormats
+    {
+    }
+
+    public unsafe struct AVFilterChannelLayouts
+    {
+    }
+
+    public unsafe struct AVFilterGraphInternal
+    {
+    }
+
+    public unsafe struct AVFilterInternal
+    {
+    }
+
+    public unsafe struct AVFilterCommand
+    {
+    }
+
+}
